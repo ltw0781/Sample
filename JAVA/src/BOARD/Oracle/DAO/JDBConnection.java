e7681a8dbb0bc98b87704250ae8812d596c6e16a
ltw0781 <ltw0781@naver.com>
2024-04-25 18:16:36 +0900

    2024-04-25 집에서

--- JAVA/src/BOARD/Oracle/DAO/JDBConnection.java
+++ JAVA/src/BOARD/Oracle/DAO/JDBConnection.java
@@ -1,10 +1,7 @@
 package BOARD.Oracle.DAO;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar.String;
+import com.mysql.cj.xdevapi.Statement;
 
 
 public class JDBConnection {
